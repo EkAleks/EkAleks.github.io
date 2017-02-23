9d8f41002f3f8ca0a4f74aaa8b5c9e2ffe9b888c
Ekaterina Alekseeva <klem4@yandex.ru>
2017-02-23 16:35:07 +0300

    Изменила названия классов для соответствия БЭМ

--- js/ekaleks.js
+++ js/ekaleks.js
@@ -1,15 +1,17 @@
     var link = document.querySelector(".popap-certificate-img");
 	var popup = document.querySelector(".popap-certificate");
-	var close = document.querySelector(".popap-close");
+	var close = document.querySelector(".popap-certificate_close");
    
   
 	link.addEventListener("click", function(event) {
        event.preventDefault();
-	   popup.classList.add("popap-certificate-show");
+	   popup.classList.add("popap-certificate_show");
       });
 	close.addEventListener("click", function(event) {
         event.preventDefault();
-        popup.classList.remove("popap-certificate-show");
+        popup.classList.remove("popap-certificate_show");
    });
+   
+   
         
   
