907eded58452153123021003cc987b3917d5ac84
Алексеева Екатерина <klem4@yandex.ru>
2018-11-05 23:14:50 +0300

    Поправила попап

--- js/ekaleks.js
+++ js/ekaleks.js
@@ -3,15 +3,16 @@
 	var close = document.querySelector(".popap-certificate_close");
    
   
-	link.addEventListener("click", function(event) {
-       event.preventDefault();
+	link.addEventListener("click", function(evt) {
+       evt.preventDefault();
 	   popup.classList.add("popap-certificate_show");
       });
-	close.addEventListener("click", function(event) {
-        event.preventDefault();
+	close.addEventListener("click", function(evt) {
+        evt.preventDefault();
         popup.classList.remove("popap-certificate_show");
    });
-   
-   
-        
-  
+   document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      popup.classList.remove('popap-certificate_show');
+    }
+  });
