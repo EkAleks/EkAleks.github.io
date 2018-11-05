146e76bae5894feb1c9233e5cbbcdc115190cac4
Алексеева Екатерина <klem4@yandex.ru>
2018-11-05 23:23:03 +0300

    Небольшое изменение

--- js/ekaleks.js
+++ js/ekaleks.js
@@ -3,16 +3,16 @@
 	var close = document.querySelector(".popap-certificate_close");
    
   
-	link.addEventListener("click", function(evt) {
-       evt.preventDefault();
-	   popup.classList.add("popap-certificate_show");
-      });
-	close.addEventListener("click", function(evt) {
-        evt.preventDefault();
+    link.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      popup.classList.add("popap-certificate_show");
+    });
+    close.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      popup.classList.remove("popap-certificate_show");
+    });
+    document.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
         popup.classList.remove("popap-certificate_show");
-   });
-   document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
-      popup.classList.remove('popap-certificate_show');
-    }
-  });
+      }
+    });
